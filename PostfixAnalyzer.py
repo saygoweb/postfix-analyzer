7f279c66c58a06994eed5febc3d2af43e5bbcee5
Cambell <cambell.prince@gmail.com>
2020-06-15 04:09:46 +0000

    Update Python 3

--- PostfixAnalyzer.py
+++ PostfixAnalyzer.py
@@ -5,7 +5,7 @@ import re
 import sys
 
 def usage():
-    print 'PostfixAnalyzer -f filename'
+    print('PostfixAnalyzer -f filename')
 
 class PostfixLine:
     _name = ''
@@ -105,7 +105,7 @@ class PostfixAnalyzer:
     def onNoQueue(self, line):
         ss = line.split(None, 7)
         pid = self.getInside(ss[4], '[', ']')
-        if self._pidSMTPDMap.has_key(pid):
+        if pid in self._pidSMTPDMap:
             transaction = self._pidSMTPDMap[pid]
             infoColon = ss[7].split(':', 1)
             posFrom = infoColon[1].find('from=')
@@ -117,17 +117,17 @@ class PostfixAnalyzer:
             transaction._status = 'noqu'
             self.report(transaction)
         else:
-            print 'Warn: Process %s not found in onNoQueue' % pid
+            print('Warn: Process %s not found in onNoQueue' % pid)
 
     def onQueue(self, line):
         ss = line.split(None, 7)
         pid = self.getInside(ss[4], '[', ']')
-        if self._pidSMTPDMap.has_key(pid):
+        if pid in self._pidSMTPDMap:
             transaction = self._pidSMTPDMap[pid]
             transaction._postfixID = ss[5].rstrip(':')
             self._postfixIDMap[transaction._postfixID] = transaction
         else:
-            print 'Warn: Process %s not found in onQueue' % pid
+            print('Warn: Process %s not found in onQueue' % pid)
 
     def onCleanup(self, line):
         ss = line.split(None, 7)
@@ -139,49 +139,51 @@ class PostfixAnalyzer:
             messageID = ss[6][posEqual + 1:]
             if messageID[0] == '<':
                 messageID = self.getInside(messageID, '<', '>')
-            if self._postfixIDMap.has_key(postfixID):
+            if postfixID in self._postfixIDMap:
                 transaction = self._postfixIDMap[postfixID]
                 transaction._messageID = messageID
                 self._messageIDMap[messageID] = transaction
-            elif self._messageIDMap.has_key(messageID):
+            elif messageID in self._messageIDMap:
                 transaction = self._messageIDMap[messageID]
                 transaction._postfixIDDelivery = postfixID
                 self._postfixIDMap[postfixID] = transaction
         else:
-            print "Error: Malformed messageID in onCleanup '%s'" % ss[6]
+            if ss[6] != 'discard:' and ss[6] != 'warning:':
+                print("Error: Malformed messageID in onCleanup '%s'" % ss[6])
     
     def onQueued(self, line):
         ss = line.split(None, 6)
         postfixID = ss[5].rstrip(':')
-        if self._postfixIDMap.has_key(postfixID):
+        if postfixID in self._postfixIDMap:
             transaction = self._postfixIDMap[postfixID]
             info = ss[6].split(',')
             transaction._from = self.getInside(info[0], '<', '>')
             transaction._size = info[1][6:]
         #else:
-        #    print 'Error: Transaction %s not found in onQueued' % postfixID
+        #    () 'Error: Transaction %s not found in onQueued' % postfixID)
 
     def onPipe(self, line):
         ss = line.split(None, 6)
         postfixID = ss[5].rstrip(':')
-        if self._postfixIDMap.has_key(postfixID):
+        if postfixID in self._postfixIDMap:
             transaction = self._postfixIDMap[postfixID]
             info = ss[6].split(',')
             transaction._to = self.getInside(info[0], '<', '>')
             transaction._delay = info[3][7:]
+            transaction._status = 'come'
         #else:
-        #    print 'Error: Transaction %s not found in onQueued' % postfixID
+        #    print('Error: Transaction %s not found in onPipe' % postfixID)
 
     def onSpam(self, line):
         ss = line.split(None, 12)
         pidSpam = self.getInside(ss[4], '[', ']')
         if ss[5] == 'processing':
             messageID = self.getInside(ss[7], '<', '>')
-            if self._messageIDMap.has_key(messageID):
+            if messageID in self._messageIDMap:
                 transaction = self._messageIDMap[messageID]
                 self._pidSpamMap[pidSpam] = transaction
             else:
-                print "Error: Malformed messageID <%s>" % (messageID)
+                print("Error: Malformed messageID <%s>" % (messageID))
         elif ss[5] == 'clean' or ss[5] == 'identified':
             transaction = self._pidSpamMap[pidSpam]
             score = self.getInside(ss[7], '(', ')')
@@ -196,65 +198,68 @@ class PostfixAnalyzer:
     def onSMTP(self, line):
         ss = line.split(None, 6)
         postfixID = ss[5].rstrip(':')
-        if self._postfixIDMap.has_key(postfixID):
+        if postfixID in self._postfixIDMap:
             transaction = self._postfixIDMap[postfixID]
             info = ss[6].split(',')
             transaction._to = self.getInside(info[0], '<', '>')
             transaction._relay = info[1][7:]
             transaction._delay = info[2][7:]
-            transaction._status = 'done'
+            transaction._status = 'gone'
             transaction._result = info[3][8:]
             transaction._transport = 'smtp'
         else:
-            print 'Error: Transaction %s not found in onSMTP' % postfixID
+            print('Error: Transaction %s not found in onSMTP' % postfixID)
         
     def onVirtual(self, line):
         ss = line.split(None, 6)
         postfixID = ss[5].rstrip(':')
-        if self._postfixIDMap.has_key(postfixID):
+        if postfixID in self._postfixIDMap:
             transaction = self._postfixIDMap[postfixID]
             info = ss[6].split(',')
             transaction._to = self.getInside(info[0], '<', '>')
             transaction._relay = info[1][7:]
             transaction._delay = info[2][7:]
-            transaction._status = 'done'
+            transaction._status = 'gone'
             transaction._result = info[3][8:]
             transaction._transport = 'virtual'
         else:
-            print 'Error: Transaction %s not found in onVirtual' % postfixID
+            print('Error: Transaction %s not found in onVirtual' % postfixID)
         
     def onRemoved(self, line):
         ss = line.split(None, 6)
         postfixID = ss[5].rstrip(':')
-        if self._postfixIDMap.has_key(postfixID):
+        if postfixID in self._postfixIDMap:
             transaction = self._postfixIDMap[postfixID]
             if postfixID == transaction._postfixIDDelivery:
                 # The final delivery
                 self.report(transaction)
             elif transaction._status == 'spam':
                 self.report(transaction)
-        #else:
-        #    print 'Error: Transaction %s not found in onRemoved' % postfixID
+            else:
+                # print('Error: Transaction %s not final in onRemoved' % postfixID)
+                self.report(transaction)
+        else:
+            print('Error: Transaction %s not found in onRemoved' % postfixID)
         
     def report(self, tt):
         if self._reporter != None:
             self._reporter(tt)
 
 def summaryReport(tt):
-    print "%s id:%s score:%s %s to:%s from:%s" % (tt._status, tt._postfixID, tt._spamScore, tt._transport, tt._to, tt._from)
+    print("%s id:%s %s to:%s from:%s" % (tt._status, tt._postfixID, tt._transport, tt._to, tt._from))
 
 def spamSummaryReport(tt):
     reason = "%s %s" % (tt._spamReport, tt._result)
-    print "%s %s id:%s score:%s %s to:%s from:%s %s" % (tt._dateTime, tt._status, tt._postfixID, tt._spamScore, tt._transport, tt._to, tt._from, reason)
+    print("%s %s id:%s score:%s %s to:%s from:%s %s" % (tt._dateTime, tt._status, tt._postfixID, tt._spamScore, tt._transport, tt._to, tt._from, reason))
 
 def detailReport(tt):
-    print "%s id: %s to: %s from: %s" % (tt_.dateTime, tt._postfixID, tt._to, tt._from)
-    print "  status: %s" % tt._status
-    print "  result: %s" % tt._result
-    print "  spam score: %s" % tt._spamScore
-    print "  spam delay: %s" % tt._spamDelay
-    print "  spam report: %s" % tt._spamReport
-    print "  host: %s" % tt._host
+    print("%s id: %s to: %s from: %s" % (tt_.dateTime, tt._postfixID, tt._to, tt._from))
+    print("  status: %s" % tt._status)
+    print("  result: %s" % tt._result)
+    print("  spam score: %s" % tt._spamScore)
+    print("  spam delay: %s" % tt._spamDelay)
+    print("  spam report: %s" % tt._spamReport)
+    print("  host: %s" % tt._host)
 
 def main(argv):
     filePath = '/var/log/mail.log'
@@ -265,7 +270,7 @@ def main(argv):
         sys.exit(2)
 
     app = PostfixAnalyzer()
-    app._reporter = spamSummaryReport
+    app._reporter = summaryReport
     for opt, arg in opts:
         if opt in ('-f', '--file'):
             filePath = arg
